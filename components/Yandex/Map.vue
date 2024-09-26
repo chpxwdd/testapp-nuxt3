d82ace5d88b382643dd8161cc28ffee7b1bf088b
chpxwdd <chpxwdd@gmail.com>
2024-09-26 16:50:02 +0300

    b

--- components/Yandex/Map.vue
+++ components/Yandex/Map.vue
@@ -1,63 +1,33 @@
 <template>
   <div>
-    <yandex-map
-      :settings="{
-        location: {
-          center: [hotels[0].lng, hotels[0].lat],
-          zoom: 9,
-          duration: 0,
-        },
-      }"
-      height="1000px"
-      width="100%"
-    >
+    <yandex-map height="600px" width="100%" :settings="{
+      location: {
+        center: [hotels[0].lng, hotels[0].lat],
+        zoom: 9,
+        duration: 0,
+      }
+    }">
       <yandex-map-default-scheme-layer />
       <yandex-map-default-features-layer />
-      <!-- <yandex-map-default-marker
-        :settings="{
-          coordinates: [hotels[0].lng, hotels[0].lat],
-          title: 'Marker with static content',
-          subtitle: 'Marker with static content description',
-          color: 'green',
-          popup: {
-            position: 'top',
-            content: 'Static content that hides marker',
-            hidesMarker: true,
-          },
-        }"
-      >
-        <template #popup="{ close }">
-          <div class="marker-popup" @click="close()">
-            Click me to close popup
-          </div>
-        </template>
-      </yandex-map-default-marker> -->
 
       <template v-for="(marker, markerIndex) in hotels" :key="markerIndex">
-        <yandex-map-marker
-          v-if="marker.lng || marker.lat"
-          :settings="{
-            onClick: () => (openMarker = markerIndex),
-            zIndex: openMarker === markerIndex ? 1 : 0,
-            coordinates: [marker.lng, marker.lat],
-            title: `от ${marker.priceFrom} ${marker.currency}`,
-            properties: {
-              priceFrom: marker.priceFrom,
-              currency: marker.currency,
-            },
-            popup: { position: 'top' },
-          }"
-        >
+        <yandex-map-marker v-if="marker.lng || marker.lat" :settings="{
+          onClick: () => (openMarker = markerIndex),
+          zIndex: openMarker === markerIndex ? 1 : 0,
+          coordinates: [marker.lng, marker.lat],
+          title: `от ${marker.priceFrom} ${marker.currency}`,
+          properties: {
+            priceFrom: marker.priceFrom,
+            currency: marker.currency,
+          },
+          popup: { position: 'top' },
+        }">
           <div class="hotel">
             от {{ formatNumberWithSpaces(marker.priceFrom) }}
             {{ marker.currency }}
 
-            <div
-              v-if="openMarker === markerIndex"
-              class="popup"
-              @click.stop="openMarker = null"
-            >
-              Click me to close popup
+            <div v-if="openMarker === markerIndex" class="popup" @click.stop="openMarker = null">
+              {{ marker.name }}
             </div>
           </div>
 
@@ -124,44 +94,6 @@ const handleCluster = (cl) => {
 </script>
 
 <style scoped lang="scss">
-.marker-popup {
-  background: #fff;
-  border-radius: 3px;
-  border: #393939;
-  padding: 10px;
-  color: black;
-  cursor: pointer;
-  font-size: 14px;
-  white-space: nowrap;
-}
-.popup {
-  position: relative;
-  top: calc(100% + 3px);
-  width: 200px;
-  border-radius: 3px;
-  border: #393939;
-  background: #ddd;
-  border-radius: 3px;
-  padding: 3px;
-  font-size: 10px;
-  color: black;
-  text-align: left;
-}
-.marker {
-  background: green;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 10px;
-  cursor: pointer;
-  color: #fff;
-  padding: 10px 20px;
-  white-space: nowrap;
-}
-
-.hotel:hover {
-  background: #d3d3d3;
-}
 .hotel {
   position: relative;
   background: white;
@@ -170,11 +102,30 @@ const handleCluster = (cl) => {
   text-align: left;
   color: #393939;
   font-size: 0.6rem;
+  font-weight: bold;
   // width: 100px;
   text-align: left;
-  padding: 0.5rem;
+  padding: 0.25rem;
   border: 1px solid #999;
   border-radius: 3px;
   cursor: pointer;
+}
+
+.hotel:hover {
+  background: #e3e3d3;
+}
+
+.popup {
+  position: relative;
+  top: calc(100% + 3px);
+  border-top: 1px solid #ccc;
+  // background: #ddd;
+  margin-top: 0.25rem;
+  padding: 0.15rem 0.25rem;
+  font-size: 10px;
+  color: black;
+  text-align: left;
+  font-weight: normal;
+  white-space: nowrap;
 }
 </style>
