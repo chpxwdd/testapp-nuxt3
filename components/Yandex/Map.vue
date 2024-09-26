6cbc069ef6b34270ad8aa808944d5d9f7d6c7d58
chpxwdd <chpxwdd@gmail.com>
2024-09-26 16:24:28 +0300

    add markers with popups to yandex-map-vue

--- components/Yandex/Map.vue
+++ components/Yandex/Map.vue
@@ -13,25 +13,59 @@
     >
       <yandex-map-default-scheme-layer />
       <yandex-map-default-features-layer />
+      <!-- <yandex-map-default-marker
+        :settings="{
+          coordinates: [hotels[0].lng, hotels[0].lat],
+          title: 'Marker with static content',
+          subtitle: 'Marker with static content description',
+          color: 'green',
+          popup: {
+            position: 'top',
+            content: 'Static content that hides marker',
+            hidesMarker: true,
+          },
+        }"
+      >
+        <template #popup="{ close }">
+          <div class="marker-popup" @click="close()">
+            Click me to close popup
+          </div>
+        </template>
+      </yandex-map-default-marker> -->
 
       <template v-for="(marker, markerIndex) in hotels" :key="markerIndex">
         <yandex-map-marker
           v-if="marker.lng || marker.lat"
           :settings="{
-            onClick: handleClick,
-            zIndex:-1000,
+            onClick: () => (openMarker = markerIndex),
+            zIndex: openMarker === markerIndex ? 1 : 0,
             coordinates: [marker.lng, marker.lat],
             title: `от ${marker.priceFrom} ${marker.currency}`,
             properties: {
               priceFrom: marker.priceFrom,
               currency: marker.currency,
             },
+            popup: { position: 'top' },
           }"
         >
           <div class="hotel">
-            <!-- <div class="name">{{ marker.name }}</div> -->
-            <div class="priceFrom">{{ formatNumberWithSpaces(marker.priceFrom) }}</div>
+            от {{ formatNumberWithSpaces(marker.priceFrom) }}
+            {{ marker.currency }}
+
+            <div
+              v-if="openMarker === markerIndex"
+              class="popup"
+              @click.stop="openMarker = null"
+            >
+              Click me to close popup
+            </div>
           </div>
+
+          <!-- <div class="hotel">
+            <div class="priceFrom">
+              {{ formatNumberWithSpaces(marker.priceFrom) }}
+            </div>
+          </div> -->
         </yandex-map-marker>
       </template>
     </yandex-map>
@@ -72,6 +106,8 @@ const hintVisible = shallowRef(false);
 const isLoading = shallowRef(true);
 const isLocationSelected = shallowRef(false);
 
+const openMarker = ref(null);
+
 const openHotelCard = (index, cords) => {
   activeHotel.value = index;
   isLocationSelected.value = true;
@@ -88,22 +124,58 @@ const handleCluster = (cl) => {
 </script>
 
 <style scoped lang="scss">
+.marker-popup {
+  background: #fff;
+  border-radius: 3px;
+  border: #393939;
+  padding: 10px;
+  color: black;
+  cursor: pointer;
+  font-size: 14px;
+  white-space: nowrap;
+}
+.popup {
+  position: relative;
+  top: calc(100% + 3px);
+  width: 200px;
+  border-radius: 3px;
+  border: #393939;
+  background: #ddd;
+  border-radius: 3px;
+  padding: 3px;
+  font-size: 10px;
+  color: black;
+  text-align: left;
+}
+.marker {
+  background: green;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  cursor: pointer;
+  color: #fff;
+  padding: 10px 20px;
+  white-space: nowrap;
+}
+
+.hotel:hover {
+  background: #d3d3d3;
+}
 .hotel {
   position: relative;
   background: white;
-  border: 2px solid #fff;
+  border: 1px solid #fff;
+
+  white-space: nowrap;
   text-align: left;
   color: #393939;
   font-size: 0.6rem;
-  width: 60px;
-  text-align: center;
+  // width: 100px;
+  text-align: left;
   padding: 2px;
-  border:1px solid #999;
+  border: 1px solid #999;
   border-radius: 3px;
-
-  .name {
-  }
-  .price {
-  }
+  cursor: pointer;
 }
 </style>
